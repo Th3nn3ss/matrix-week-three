f9a965dea6b488ac02c5417f7aba64956c98cfe4
Th3nn3ss <chuksmcdennis@yahoo.com>
2021-06-17 02:57:56 +0100

    Completed task and conducted tests

--- src/email_parser.py
+++ src/email_parser.py
@@ -2,14 +2,23 @@ import re
 
 
 class EmailParser:
-    # regex pattern that validate email
-    pattern = r''
-    keys = ['username', 'domain']
+    def __init__(self):
+        # regex pattern that validate email
+        self.pattern = r'^[A-Za-z][A-Za-z0-9+]+@[A-Za-z][A-Za-z0-9]+\.com$'
+        self.keys = ['username', 'domain']
 
     def parse(self, email):
         ''' parse email and return a dictionary of username and domain if valid and return None otherwise'''
         # Add implementation here ...
+        if re.match(self.pattern, email):
+            username, domain = email.split('@')
+            return self.convert_to_dict(username, domain)
+        else:
+            return None
 
-    def convert_to_dict(self):
+    def convert_to_dict(self, username, domain):
         '''
           Takes in two lists as params and return them as a dictionary.'''
+        result = {}
+        result[self.keys[0]], result[self.keys[1]] = username, domain
+        return result
--- tests/test_email_parser.py
+++ tests/test_email_parser.py
@@ -1,3 +1,43 @@
 # Import all required modules
 
 from unittest import TestCase
+from src.email_parser import EmailParser
+
+
+class TestEmailParser(TestCase):
+
+    def setUp(self):
+        self.test_case = EmailParser()
+
+    def test_parse(self):
+        self.assertEqual(self.test_case.parse(
+            'johndoe@gmail.com'), {"username": "johndoe", "domain": "gmail.com"})
+
+    def test_parse_2(self):
+        self.assertEqual(self.test_case.parse('john@doe@gmail.2com'), None)
+
+    def test_parse_3(self):
+        self.assertEqual(self.test_case.parse(
+            'john+doe@gmail.com'), {"username": "john+doe", "domain": "gmail.com"})
+
+    def test_parse_4(self):
+        self.assertEqual(self.test_case.parse(
+            'williem@yahoo.com'), {"username": "williem", "domain": "yahoo.com"})
+
+    def test_parse_5(self):
+        self.assertEqual(self.test_case.parse('2johndoe@gmail.2com'), None)
+
+    def test_parse_6(self):
+        self.assertEqual(self.test_case.parse('abraham_doe@gmail.com'), None)
+
+    def test_parse_7(self):
+        self.assertEqual(self.test_case.parse('+eze@gmail.com'), None)
+
+    def test_parse_8(self):
+        self.assertEqual(self.test_case.parse('actionbitters+gmail.com'), None)
+
+    def test_parse_9(self):
+        self.assertEqual(self.test_case.parse('muza iza@xyz.com'), None)
+
+    def test_parse_10(self):
+        self.assertEqual(self.test_case.parse('manofGod.com'), None)
